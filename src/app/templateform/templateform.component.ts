3be35083d59d7ffa0fbdb17ae762e4628a6610a3
vishalant23 <vishal.pawar@antllp.com>
2024-03-20 16:45:20 +0530

    update, add template driven form

--- src/app/templateform/templateform.component.ts
+++ src/app/templateform/templateform.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-templateform',
   templateUrl: './templateform.component.html',
   styleUrl: './templateform.component.scss'
 })
-export class TemplateformComponent {
+export class TemplateformComponent implements OnInit{
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(form:any){
+    if(form.valid){
+      console.log('Form submitted!', form.value);
+    }else{
+      console.log('Form not submitted!', form.value);
+    }
+  }
 
 }
